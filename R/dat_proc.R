61e58f9ba6e8f40d036d6ca532d238c175ae69c1
fawda123 <mbafs2012@gmail.com>
2018-02-28 14:43:43 -0800

    simple eval done

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,17 +1,20 @@
 library(readxl)
 library(tidyverse)
 
-envdat <- read_excel('C:/Users/Marcus.SCCWRP2K/Desktop/MeanSurface100m2016WCOA.xlsx') %>% 
+envdat <- read_excel('raw/MeanSurface100m2016WCOA.xlsx') %>% 
   select(`CTD Station`, `'pCO2'`, `'Aragonite'`, `'Oxygen'`, `'Temperature'`, `'Salinity'`, `'Alkalinity'`) %>% 
   rename(
     CTD = `CTD Station`, 
     Ara = `'Aragonite'`, 
     O2 = `'Oxygen'`,
-    pCO2 = `pCO2`,
+    pCO2 = `'pCO2'`,
     Temp = `'Temperature'`, 
     Sal = `'Salinity'`, 
     TA = `'Alkalinity'`
   )
-biodat <- read_excel('C:/Users/Marcus.SCCWRP2K/Desktop/PteropodIntegrated_oxibiomarkers.xlsx', 
+biodat <- read_excel('raw/PteropodIntegrated_oxibiomarkers.xlsx', 
                      sheet = 'Pteropod30mbis') %>% 
-  select(CTD, GSHonGSSG, GST, GR, LPX, CAT, ORAC, SOC)
+  select(CTD, GSHonGSSG, GST, GR, LPX, CAT, ORAC, SOD)
+
+save(envdat, file = 'data/envdat.RData', compress = 'xz')
+save(biodat, file = 'data/biodat.RData', compress = 'xz')
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # README
+
 Materials for evaluating pteropod biomarksers
+
+## Files
+
+`data/envdat.RData` Environmental data
+`data/biodat.RData` Biology data
