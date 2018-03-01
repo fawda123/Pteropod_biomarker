b026301cccce6766a01717b4be528655f622b0d5
fawda123 <mbafs2012@gmail.com>
2018-02-28 16:53:24 -0800

    new env vars, biomark ratio

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,11 +2,14 @@ library(readxl)
 library(tidyverse)
 
 envdat <- read_excel('raw/MeanSurface100m2016WCOA.xlsx') %>% 
-  select(`CTD Station`, `'pCO2'`, `'Aragonite'`, `'Oxygen'`, `'Temperature'`, `'Salinity'`, `'Alkalinity'`) %>% 
+  select(`CTD Station`, `'pCO2'`, `'pH'`, `'CO3'`, `'Fluorescence'`, `'Aragonite'`, `'Oxygen'`, `'Temperature'`, `'Salinity'`, `'Alkalinity'`) %>% 
   rename(
     CTD = `CTD Station`, 
     Ara = `'Aragonite'`, 
     O2 = `'Oxygen'`,
+    pH = `'pH'`,
+    CO3 = `'CO3'`,
+    Fluor = `'Fluorescence'`,
     pCO2 = `'pCO2'`,
     Temp = `'Temperature'`, 
     Sal = `'Salinity'`, 
@@ -14,7 +17,10 @@ envdat <- read_excel('raw/MeanSurface100m2016WCOA.xlsx') %>%
   )
 biodat <- read_excel('raw/PteropodIntegrated_oxibiomarkers.xlsx', 
                      sheet = 'Pteropod30mbis') %>% 
-  select(CTD, GSHonGSSG, GST, GR, LPX, CAT, ORAC, SOD)
+  select(CTD, GSHonGSSG, GST, GR, LPX, CAT, ORAC, SOD, Latitude) %>% 
+  mutate(
+    ORACvLPX = ORAC / LPX
+  )
 
 save(envdat, file = 'data/envdat.RData', compress = 'xz')
 save(biodat, file = 'data/biodat.RData', compress = 'xz')
