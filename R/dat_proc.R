323761c8ec6db6e91ebab2da6cf6fe7e537f518c
fawda123 <mbafs2012@gmail.com>
2018-03-23 15:07:26 -0700

    stressor mods created in dat_proc

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(tidyverse)
 
+source('R/funcs.R')
+
 # environmental data
 envdat <- read_excel('raw/MeanSurface100m2016WCOA.xlsx') %>% 
   select(`CTD Station`, `Latitude`, `'pCO2'`, `'pH'`, `'CO3'`, `'Fluorescence'`, `'Aragonite'`, `'Oxygen'`, `'Temperature'`, `'Salinity'`, `'Alkalinity'`) %>% 
@@ -114,3 +116,177 @@ expdat <- read_excel('raw/experimental treatment for Marcus.xlsx') %>%
   ungroup
 
 save(expdat, file = 'data/expdat.RData', compress = 'xz')
+
+######
+# all stressor covar models
+
+##
+# cellular
+
+# data
+data(envdat)
+data(ptedat)
+
+envchr <- c('pCO2', 'pH', 'CO3', 'Ara', 'O2', 'Temp', 'Fluor')
+ptechr <- c('CAT', 'GR', 'GSHonGSSG', 'GST', 'LPX', 'ORAC', 'SOD', 'ORACvLPX')
+
+dat_frm <- ptedat %>% 
+  select(one_of('CTD', ptechr)) %>% 
+  na.omit %>% 
+  inner_join(envdat, by = 'CTD') %>% 
+  select(one_of(c('CTD', envchr, ptechr))) %>% 
+  data.frame %>% 
+  remove_rownames %>% 
+  column_to_rownames('CTD')
+
+# models
+env_cmb <- envchr %>% 
+  combn(2) %>% 
+  t %>% 
+  data.frame(stringsAsFactors = F) %>%
+  crossing(ptechr, .) %>% 
+  t %>% 
+  data.frame(stringsAsFactors = F) %>% 
+  as.list
+
+# models
+mod_all <- env_cmb %>%
+  map(function(x, dat = dat_frm){
+    
+    # formula
+    frm <- paste(x[1], '~', x[2], '*', x[3]) %>% 
+      as.formula
+    
+    # model only
+    lm(frm, data = dat)
+    
+  }) %>% 
+  enframe('Model', 'Modobj') %>% 
+  mutate(
+    Model = gsub('^X', 'mod', Model), 
+    data = map(Modobj, function(x){
+      
+      # response
+      yvar <- all.vars(formula(x))[1]
+      
+      # rsq
+      rsq <- summary(x) %>% 
+        .$r.squared
+      
+      # coeff summary
+      out <- x %>% 
+        summary %>% 
+        .$coefficients %>% 
+        .[-1, c(1, 4)] %>% 
+        data.frame %>% 
+        rownames_to_column('env_lab') %>% 
+        mutate(
+          pte_lab = yvar,
+          Rsq = round(rsq, 2),
+          Est = round(Estimate, 2), 
+          Pvl = p_ast(Pr...t..)
+        ) %>% 
+        select(pte_lab, env_lab, Rsq, Est, Pvl)
+      
+      return(out)
+      
+    })
+  ) 
+
+biomod <- mod_all
+save(biomod, file = 'data/biomod.RData', compress = 'xz')
+
+##
+# physiological/abundance
+
+# data
+data(envdat)
+data(ptedat)
+
+envchr <- c('pCO2', 'pH', 'CO3', 'Ara', 'O2', 'Temp', 'Fluor')
+ptechr <- c('abu', 'dis', 'len', 'ty2', 'ty3', 'scr')
+
+dat_frm <- ptedat %>% 
+  select(one_of('CTD', ptechr)) %>% 
+  left_join(envdat, by = 'CTD') %>% 
+  data.frame %>% 
+  remove_rownames %>% 
+  column_to_rownames('CTD')
+
+# models
+
+# variables to model, chr strings only
+env_cmb <- envchr %>% 
+  combn(2) %>% 
+  t %>% 
+  data.frame(stringsAsFactors = F) %>%
+  crossing(ptechr, .) %>% 
+  t %>% 
+  data.frame(stringsAsFactors = F) %>% 
+  as.list
+
+# # back-transform abu for models
+# dat_frm <- dat_frm %>% 
+#   mutate(
+#     abu = (10^abu) - 1,
+#     abu = as.integer(round(abu, 0))
+#   )
+#   
+# models
+mod_all <- env_cmb %>%
+  
+  map(function(x, dat = dat_frm){
+
+    # formula
+    frm <- paste(x[1], '~', x[2], '*', x[3]) %>% 
+      as.formula
+    
+    # if(x[1] == 'abu'){
+    #   
+    #   # zinf model
+    #   out <- zeroinfl(frm, data = dat_frm)
+    # 
+    # } else {
+    
+    # model only
+    out <- lm(frm, data = dat)
+    
+    # }
+    
+    return(out)
+    
+  }) %>% 
+  enframe('Model', 'Modobj') %>% 
+  mutate(
+    Model = gsub('^X', 'mod', Model), 
+    data = map(Modobj, function(x){
+      
+      # response
+      yvar <- all.vars(formula(x))[1]
+      
+      # rsq
+      rsq <- summary(x) %>% 
+        .$r.squared
+      
+      # coeff summary
+      out <- x %>% 
+        summary %>% 
+        .$coefficients %>% 
+        .[-1, c(1, 4)] %>% 
+        data.frame %>% 
+        rownames_to_column('env_lab') %>% 
+        mutate(
+          pte_lab = yvar,
+          Rsq = round(rsq, 2),
+          Est = round(Estimate, 2), 
+          Pvl = p_ast(Pr...t..)
+        ) %>% 
+        select(pte_lab, env_lab, Rsq, Est, Pvl)
+      
+      return(out)
+      
+    })
+  ) 
+
+phymod <- mod_all
+save(phymod, file = 'data/phymod.RData', compress = 'xz')
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # README
 
-Materials for evaluating pteropod biomarkers ([link](http://162.243.131.102:3838/Pteropod_biomarker/biomark.Rmd)),
-physiology ([link](http://162.243.131.102:3838/Pteropod_biomarker/physio.Rmd)) in response to OA
+Materials for evaluating pteropod cellular biomarkers ([link](http://162.243.131.102:3838/Pteropod_biomarker/biomark.Rmd)),
+physiology ([link](http://162.243.131.102:3838/Pteropod_biomarker/physio.Rmd)) in response to OA, figures [here](https://fawda123/github.io/Pteropod_biomarker/figures.html)
 
 ## Files
 
@@ -9,4 +9,8 @@ physiology ([link](http://162.243.131.102:3838/Pteropod_biomarker/physio.Rmd)) i
 
 `data/biodat.RData` Biology data
 
+`data/biomod.RData` Stressor covariance models for cellular/biomarkers
+
 `data/expdat.RData` Exposure data
+
+`data/phymod.RData` Stressor covariance models for physiology/abundance
